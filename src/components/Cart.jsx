9890aa49e91838ca5ac6b08d15b4cd03851d2bac
Nazar Koval <n.kovaldev@gmail.com>
2023-03-15 05:36:18 +0200

    ref: close Alert when basket is opening

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,10 +2,16 @@ import { useContext } from "react"
 import { ShopContext } from "../context"
 
 const Cart = props => {
-	const { order, handleBasketShow } = useContext(ShopContext)
-   const quantity = order.length
+	const { order, handleBasketShow, closeAlert } = useContext(ShopContext)
+	const quantity = order.length
 	return (
-		<div className="cart blue darken-4 white-text" onClick={handleBasketShow}>
+		<div
+			className="cart blue darken-4 white-text"
+			onClick={() => {
+				handleBasketShow()
+            closeAlert()
+			}}
+		>
 			<i className="material-icons">shopping_cart</i>
 			{quantity ? <span className="cart-quantity">{quantity}</span> : null}
 		</div>
